aa385f4bb2ffa9b0186972604ebfb16f4712ccb7
Jiasheng <spy_yjs@hotmail.com>
2015-09-06 16:32:15 +0800

    major updates to app; coded dynamically changing categoryactivity and entryactivity

---
+++ app/src/main/java/confirmcanstudios/settlinginsingapore/EntryActivity.java
@@ -0,0 +1,61 @@
+package confirmcanstudios.settlinginsingapore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class EntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        
+        dynamicEntrySet();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void dynamicEntrySet(){
+        String imageName = getIntent().getStringExtra("Entry").toLowerCase().replace(" ", "");
+
+        //Gets the imageID based on the selected word/phrase
+        int dynamicImageID = getResources().getIdentifier(imageName , "drawable",
+                this.getPackageName());
+
+        int dynamicStringID = getResources().getIdentifier(getIntent().getStringExtra("Entry").replace(" ", ""), "string",
+                this.getPackageName());
+
+        ImageView EntryImage = (ImageView) findViewById(
+                R.id.EntryActivityImage);
+        TextView EntryTitle = (TextView) findViewById(
+            R.id.EntryActivityTitle);
+        TextView EntryDesc = (TextView) findViewById(
+                R.id.EntryActivityDesc);
+
+        //Programmatically set resources in EntryActivity
+        setTitle(getIntent().getStringExtra("Entry"));
+        EntryImage.setImageResource(dynamicImageID);
+        EntryTitle.setText(getIntent().getStringExtra("Entry"));
+        EntryDesc.setText(dynamicStringID);
+    }
+
+}
--- app/src/main/java/confirmcanstudios/settlinginsingapore/MainActivity.java
+++ app/src/main/java/confirmcanstudios/settlinginsingapore/MainActivity.java
@@ -38,8 +38,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //  Screen Navigation
-    public void startCategoryOne (View view){
+    /*public void startCategoryOne (View view){
         Intent intent = new Intent(this, CategoryOneActivity.class);
+        startActivity(intent);
+    }*/
+    public void CategoryOnClick(View v) {
+        switch (v.getId()) {
+            case R.id.categoryOne:
+                startActivity(R.string.category1, R.array.categoryOneItems);
+                break;
+            case R.id.categoryTwo:
+                startActivity(R.string.category2, R.array.categoryTwoItems);
+                break;
+            case R.id.categoryThree:
+                startActivity(R.string.category3, R.array.categoryThreeItems);
+                break;
+            case R.id.categoryFour:
+                startActivity(R.string.category4, R.array.categoryFourItems);
+                break;
+            case R.id.categoryFive:
+                startActivity(R.string.category5, R.array.categoryFiveItems);
+                break;
+            case R.id.categorySix:
+                startActivity(R.string.category6, R.array.categorySixItems);
+                break;
+            case R.id.categorySeven:
+                startActivity(R.string.category7, R.array.categorySevenItems);
+                break;
+            case R.id.categoryEight:
+                startActivity(R.string.category8, R.array.categoryEightItems);
+                break;
+            default:
+                break;
+        }
+    }
+    private void startActivity(int m, int n){
+        Intent intent = new Intent(this, CategoryActivity.class);
+        intent.putExtra("CategoryID", m);
+        intent.putExtra("CategoryArrayID", n);
         startActivity(intent);
     }
 
